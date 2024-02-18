295136d73ee7a76617bae77fb26b418b9742617d
AgusPostai <agustin.postai@gmail.com>
2024-02-18 20:54:03 -0300

    form

--- tasksFront/src/components/TaskForm.tsx
+++ tasksFront/src/components/TaskForm.tsx
@@ -1,10 +1,19 @@
-
 function TaskForm() {
   return (
     <div>
-      TaskForm
+      <form action=""></form>
+      <input type="text" name="title" className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-86 block w-full my-2" placeholder="Write a title"/>
+      <textarea name="description" rows={3} className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-86 block w-full my-2 " placeholder="Write a title">
+        
+      </textarea>
+      <label htmlFor="">
+        <input type="checkbox" />
+        <span>Done</span>
+      </label>
+
+      <button>Save</button>
     </div>
-  )
+  );
 }
 
-export default TaskForm
+export default TaskForm;
