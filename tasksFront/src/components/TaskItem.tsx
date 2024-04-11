65c39b043973ec4e0b418d75fefc620bfb6e4327
AgusPostai <agustin.postai@gmail.com>
2024-04-11 19:40:40 -0300

    backOnProject

---
+++ tasksFront/src/components/TaskItem.tsx
@@ -0,0 +1,32 @@
+import { Task } from "../interfaces/task.interface";
+import { useTasks } from "../context/useTasks";
+
+interface Props {
+  task: Task;
+}
+
+function TaskItem({ task }: Props) {
+  const { deleteTask } = useTasks();
+  return (
+    <div
+      key={task._id}
+      className="bg-gray-900 p-2 my-2 fñex justify-between hover:bg-gray-800 hover:cursor-pointer"
+    >
+      <div>
+        <h1>{task.title}</h1>
+        <p>{task.description}</p>
+      </div>
+
+      <div className="flex gap-x-2">
+        <button>update</button>
+        <button
+          onClick={async () => {
+            await deleteTask(task._id);
+          }}
+        >delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default TaskItem;
--- tasksFront/src/context/TaskContext.tsx
+++ tasksFront/src/context/TaskContext.tsx
@@ -1,15 +1,17 @@
 import { createContext, useState, useEffect } from "react";
-import { getTaskRequest, createTaskRequest} from "../api/tasks";
+import { getTaskRequest, deleteTaskRequest, createTaskRequest} from "../api/tasks";
 import { CreateTask, Task} from "../interfaces/task.interface";
 
 interface TaskContextValue {
   tasks: Task[];
-  createTask: (task: CreateTask) => void;
+  createTask: (task: CreateTask) => Promise<void>;
+deleteTask: (id: string) => Promise<void>;
 }
 
 export const TaskContext = createContext<TaskContextValue>({
   tasks: [],
-  createTask: () => {}
+  createTask: async() => {},
+  deleteTask: async() => {}
 });
 
 interface Props {
@@ -29,14 +31,21 @@ export const TaskProvider: React.FC<Props> = ({ children }) => {
      const res = await createTaskRequest(task);
      const data = await res.json();
     setTasks([...tasks, data])
+  };
+
+  const deleteTask = async (id:string) => {
+    const res = await deleteTaskRequest(id)
+  console.log(res);
+  
   }
 
-
+ 
   return (
     <TaskContext.Provider
       value={{
         tasks,
         createTask,
+        deleteTask,
       }}
     >
       {children}
